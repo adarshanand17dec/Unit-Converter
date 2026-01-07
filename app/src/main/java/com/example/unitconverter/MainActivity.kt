a025edfca1db32ce34b769ae01f50a202a217934
adarshanand17dec <adarshanad1712@gmail.com>
2026-01-07 13:01:27 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.example.unitconverter
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.unitconverter.ui.theme.UnitConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UnitConverterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    UnitConverter(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun UnitConverter(modifier: Modifier = Modifier){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+       //Here all the UI elements will be stacked below each other
+        Text("Unit Converter")
+        Spacer(modifier = Modifier.height(10.dp))
+        OutlinedTextField(value = "", onValueChange = {
+        // Here goes what should happen, when the value of OutlinedTextField change
+
+        })
+        Spacer(modifier = Modifier.height(10.dp))
+        Row {
+            //Here all the UI elements will be stacked nect to each other
+            Box{
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = {/*TODO*/}) {
+                    DropdownMenuItem(text =  {Text("Centimeter")}, onClick = { /*TODO*/})
+                    DropdownMenuItem(text =  {Text("Meter")}, onClick = { /*TODO*/})
+                    DropdownMenuItem(text =  {Text("Feet")}, onClick = { /*TODO*/})
+                    DropdownMenuItem(text =  {Text("Millimeter")}, onClick = { /*TODO*/})
+
+                }
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+            Box{
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = {/*TODO*/}) {
+                    DropdownMenuItem(text =  {Text("Centimeter")}, onClick = { /*TODO*/})
+                    DropdownMenuItem(text =  {Text("Meter")}, onClick = { /*TODO*/})
+                    DropdownMenuItem(text =  {Text("Feet")}, onClick = { /*TODO*/})
+                    DropdownMenuItem(text =  {Text("Millimeter")}, onClick = { /*TODO*/})
+
+                }
+            }
+
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Text("Result:")
+    }
+}
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun UnitConverterPreview(){
+    UnitConverter()
+}
